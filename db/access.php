c5d302294fb9785e6f72781b47e920380e24b93e
Mike Dawson <mike@gp2x.org>
2014-10-12 15:29:34 +0100

    remove tabs

--- db/access.php
+++ db/access.php
@@ -26,26 +26,26 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-	$capabilities = array(
-		'block/pcbook:myaddinstance' => array(
-			'captype' => 'write',
-			'contextlevel' => CONTEXT_SYSTEM,
-			'archetypes' => array(
-				'user' => CAP_ALLOW
-			),
-			'clonepermissionsfrom' => 'moodle/my:manageblocks'
-		),
+    $capabilities = array(
+    	'block/pcbook:myaddinstance' => array(
+    		'captype' => 'write',
+    		'contextlevel' => CONTEXT_SYSTEM,
+    		'archetypes' => array(
+    			'user' => CAP_ALLOW
+    		),
+    		'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    	),
 
-		'block/pcbook:addinstance' => array(
-			'riskbitmask' => RISK_SPAM | RISK_XSS,
-			'captype' => 'write',
-			'contextlevel' => CONTEXT_BLOCK,
-			'archetypes' => array(
-				'editingteacher' => CAP_ALLOW,
-				'manager' => CAP_ALLOW
-			),
-			'clonepermissionsfrom' => 'moodle/site:manageblocks'
-		),
-	);
+    	'block/pcbook:addinstance' => array(
+    		'riskbitmask' => RISK_SPAM | RISK_XSS,
+    		'captype' => 'write',
+    		'contextlevel' => CONTEXT_BLOCK,
+    		'archetypes' => array(
+    			'editingteacher' => CAP_ALLOW,
+    			'manager' => CAP_ALLOW
+    		),
+    		'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    	),
+    );
 
 
--- edit_form.php
+++ edit_form.php
@@ -28,26 +28,26 @@
 
 class block_pcbook_edit_form extends block_edit_form {
  
-	protected function specific_definition($mform) {
-		$mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
+    protected function specific_definition($mform) {
+    	$mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
  
-		$mform->addElement('text', 'config_uri', get_string('pcbook_uri', 'block_pcbook'));
-		$mform->setDefault('config_uri', 'http://server.example.com/pcbook');
-		$mform->setType('config_uri', PARAM_RAW);        
+    	$mform->addElement('text', 'config_uri', get_string('pcbook_uri', 'block_pcbook'));
+    	$mform->setDefault('config_uri', 'http://server.example.com/pcbook');
+    	$mform->setType('config_uri', PARAM_RAW);        
 
-		$mform->addElement('text', 'config_wstitle', get_string('pcbook_wstitle', 'block_pcbook'));
-		$mform->setDefault('config_wstitle', 'Workstation availability');
-		$mform->setType('config_wstitle', PARAM_RAW);        
+    	$mform->addElement('text', 'config_wstitle', get_string('pcbook_wstitle', 'block_pcbook'));
+    	$mform->setDefault('config_wstitle', 'Workstation availability');
+    	$mform->setType('config_wstitle', PARAM_RAW);        
 
-		$mform->addElement('text', 'config_wspattern', get_string('pcbook_wspattern', 'block_pcbook'));
-		$mform->setDefault('config_wspattern', '');
-		$mform->setType('config_wspattern', PARAM_RAW);        
+    	$mform->addElement('text', 'config_wspattern', get_string('pcbook_wspattern', 'block_pcbook'));
+    	$mform->setDefault('config_wspattern', '');
+    	$mform->setType('config_wspattern', PARAM_RAW);        
 
-		$mform->addElement('advcheckbox', 'config_showlink', get_string('pcbook_showlink', 'block_pcbook'));
-		$mform->setDefault('config_showlink', '1');
-		$mform->setType('config_showlink', PARAM_RAW);        
+    	$mform->addElement('advcheckbox', 'config_showlink', get_string('pcbook_showlink', 'block_pcbook'));
+    	$mform->setDefault('config_showlink', '1');
+    	$mform->setType('config_showlink', PARAM_RAW);        
  
-	}
+    }
 }
 
 
